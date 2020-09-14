da7746eb38b44e65d7cb3f12f2a32e37346e2397
imdreamer <743295483@qq.com>
2020-09-14 17:03:08 +0800

    [yangqian] implement: shoult return itseft when vowel less than 30%. And refactor mommifier.

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,8 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Mommifier {
 
 
-    public String convert(String vowelsMoreThan30Percent) {
+    public String convert(String string) {
 
-        return "kemommyep";
+        StringBuilder stringBuilder = new StringBuilder(string);
+
+        int vowelsCount = 0;
+        int vowelsBegin = 0;
+
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (judgeIsContinuousVowel(stringBuilder, vowelsBegin, i)) {
+                vowelsCount++;
+            } else {
+                vowelsCount = 0;
+                vowelsBegin = i + 1;
+            }
+            if (vowelsCount * 10 > stringBuilder.length() * 3) {
+                return stringBuilder.insert(vowelsBegin + 1, "mommy").toString();
+            }
+        }
+        return string;
+    }
+
+    private boolean judgeIsContinuousVowel(StringBuilder stringBuilder, int markCharacterIndex, int currentCharacterIndex) {
+
+        List<String> vowelsList = Arrays.asList("a","e","i","o","u");
+        String markCharacter = String.valueOf(stringBuilder.charAt(markCharacterIndex));
+        String currentCharacter = String.valueOf(stringBuilder.charAt(currentCharacterIndex));
+
+        return vowelsList.contains(markCharacter) && markCharacter.equals(currentCharacter);
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MommifierTest {
 
     @Test
-    void should_return_new_string_insert_mommy_when_number_of_vowels_more_than_30_percent() {
+    void should_return_new_string_insert_mommy_when_number_of_vowels_more_than_30_percent_in_string() {
 
         Mommifier mommifer = new Mommifier();
 
@@ -13,5 +13,17 @@ public class MommifierTest {
         String result = mommifer.convert(vowelsMoreThan30Percent);
 
         assertEquals("kemommyep", result);
+    }
+
+    @Test
+    void should_return_itself_when_number_of_vowels_less_than_30_percent_in_string() {
+
+        Mommifier mommifier = new Mommifier();
+
+        String vowelsLessThan30Percent = "kep";
+
+        String result = mommifier.convert(vowelsLessThan30Percent);
+
+        assertEquals("kep", result);
     }
 }
