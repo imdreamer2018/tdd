1d6044d66b5eb81977f6095b829da4db73621409
imdreamer <743295483@qq.com>
2020-09-14 17:12:21 +0800

    [yangqian] implement: should return itself when vowels not continuous.

--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -20,10 +20,22 @@ public class MommifierTest {
 
         Mommifier mommifier = new Mommifier();
 
-        String vowelsLessThan30Percent = "kep";
+        String vowelsLessThan30Percent = "keepppp";
 
         String result = mommifier.convert(vowelsLessThan30Percent);
 
-        assertEquals("kep", result);
+        assertEquals("keepppp", result);
+    }
+
+    @Test
+    void should_return_itself_when_vowels_not_continuous_in_string() {
+
+        Mommifier mommifier = new Mommifier();
+
+        String vowelsLessThan30Percent = "keap";
+
+        String result = mommifier.convert(vowelsLessThan30Percent);
+
+        assertEquals("keap", result);
     }
 }
